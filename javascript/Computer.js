1c9c32f1a5307f51735e6540938b0e2ff7a538fe
yvette <yvette@its6am.com>
2020-03-27 13:45:18 +0100

    Day 9, part 1: Sensor Boost

--- javascript/Computer.js
+++ javascript/Computer.js
@@ -1,5 +1,5 @@
 class Computer {
-    constructor(inputs, software, name) {
+    constructor(inputs, software, name, shouldLog=false) {
         this.inputs = inputs;
         this.output = null;
         this.opcode = '00';
@@ -7,16 +7,31 @@ class Computer {
         this.pointer = 0;
         this.paused = false;
         this.name = name;
+        this.relativeBase = 0;
+        this.shouldLog = shouldLog;
     }
 
     incrementPointer(incrementValue) {
         this.pointer += incrementValue;
     }
 
-    getParamValue(mode, paramImmediate, paramPosition) {
-        // Position mode: 0
-        // Immediate mode: 1
-        return mode === 1 ? paramImmediate : paramPosition; 
+    getValue(mode, param, write=false) {
+        let value;
+        // Position mode
+        if (mode === 0) value = this.software[param]; 
+
+        // Immediate mode
+        else if (mode === 1) value = param;
+
+        // Relative mode
+        else if (mode === 2) value = this.software[param + this.relativeBase]; 
+
+        if (write) {
+            value = mode === 2 ? param + this.relativeBase : param;
+        }
+
+        return value || 0;
+
     }
 
     addInput(input) {
@@ -28,13 +43,21 @@ class Computer {
             // Opcode 1:
             //      adds 2 numbers and stores result in the position of the 3rd parameter
             '01': cs => {
-                this.software[cs.storageLocation] = cs.param1value + cs.param2value;       
+                const value1 = this.getValue(cs.param1mode, cs.param1);
+                const value2 = this.getValue(cs.param2mode, cs.param2);
+                const storageLocation = this.getValue(cs.param3mode, cs.param3, true);
+
+                this.software[storageLocation] = value1 + value2;       
                 this.incrementPointer(4);
             },
             // Opcode 2:
             //      multiplies 2 numbers and stores result in the position of the 3rd parameter
             '02': cs => {
-                this.software[cs.storageLocation] = cs.param1value * cs.param2value;       
+                const value1 = this.getValue(cs.param1mode, cs.param1);
+                const value2 = this.getValue(cs.param2mode, cs.param2);
+                const storageLocation = this.getValue(cs.param3mode, cs.param3, true);
+
+                this.software[storageLocation] = value1 * value2;       
                 this.incrementPointer(4);
             },
             // Opcode 3:
@@ -44,47 +67,70 @@ class Computer {
                 if (!this.inputs.length) {
                     this.paused = true;
                 } else {
-                  const opcode3input = this.inputs.shift();
-                  this.software[cs.param1] = opcode3input;
-                  this.incrementPointer(2);
+                    const opcode3input = this.inputs.shift();
+                    this.shouldLog && console.log('input:', opcode3input);
+                    
+                    const storageLocation = this.getValue(cs.param1mode, cs.param1, true);
+                    this.software[storageLocation] = opcode3input;
+                    this.incrementPointer(2);
                 }
             },
             // Optcode 4:
-            //      outpus value of only parameter
+            //      outputs value of only parameter
             //      4,50 outputs value at address 50 (only position mode)
             '04': cs => {
-                this.output = this.software[cs.param1];
+                const value1 = this.getValue(cs.param1mode, cs.param1);
+                this.output = value1;
                 this.incrementPointer(2);
             },
             // Opcode 5:
             //      if the first parameter is non-zero, it sets the instruction pointer to the
             //      value from the second parameter. Otherwise, it does nothing.
             '05': cs => {
-                if (cs.param1value !== 0) this.pointer = cs.param2value;
+                const value1 = this.getValue(cs.param1mode, cs.param1);
+                const value2 = this.getValue(cs.param2mode, cs.param2);
+                if (value1 !== 0) this.pointer = value2;
                 else this.incrementPointer(3);
             },
             // Opcode 6:
             //      if the first parameter is zero, it sets the instruction pointer to the
             //      value from the second parameter. Otherwise, it does nothing.
             '06': cs => {
-                if (cs.param1value === 0) this.pointer = cs.param2value;
-                else this.pointer += 3;
+                const value1 = this.getValue(cs.param1mode, cs.param1);
+                const value2 = this.getValue(cs.param2mode, cs.param2);
+                if (value1 === 0) this.pointer = value2;
+                else this.incrementPointer(3);
             },
             // Opcode 7:
             //      if the first parameter is less than the second parameter, it stores 1 in
             //      the position given by the third parameter. Otherwise, it stores 0.
             '07': cs => {
-                if (cs.param1value < cs.param2value) this.software[cs.storageLocation] = 1;
-                else this.software[cs.storageLocation] = 0;
+                const value1 = this.getValue(cs.param1mode, cs.param1);
+                const value2 = this.getValue(cs.param2mode, cs.param2);
+                const storageLocation = this.getValue(cs.param3mode, cs.param3, true);
+                if (value1 < value2) this.software[storageLocation] = 1;
+                else this.software[storageLocation] = 0;
                 this.incrementPointer(4);
             },
             // Opcode 8:
             //      if the first parameter is equal to the second parameter, it stores 1 in
             //      the position given by the third parameter. Otherwise, it stores 0.
             '08': cs => {
-                if (cs.param1value === cs.param2value) this.software[cs.storageLocation] = 1;
-                else this.software[cs.storageLocation] = 0;
+                const value1 = this.getValue(cs.param1mode, cs.param1);
+                const value2 = this.getValue(cs.param2mode, cs.param2);
+                const storageLocation = this.getValue(cs.param3mode, cs.param3, true);
+                if (value1 === value2) this.software[storageLocation] = 1;
+                else this.software[storageLocation] = 0;
                 this.incrementPointer(4);
+            },
+            // Opcode 9:
+            //      adjusts the relative base by the value of its only parameter. The                 
+            //      relative base increases (or decreases, if the value is negative) by the
+            //      value of the parameter
+            '09': cs => {
+                const value1 = this.getValue(cs.param1mode, cs.param1);
+                this.relativeBase += value1;
+                this.incrementPointer(2);
             },
             // Opcode 99:
             //      program should immediately halt
@@ -104,17 +150,14 @@ class Computer {
         }
         const param1mode        = parseInt(stringInstruction.slice(2,3));
         const param2mode        = parseInt(stringInstruction.slice(1,2));
+        const param3mode        = parseInt(stringInstruction.slice(0,1));
         const opcode            = stringInstruction.slice(3,5);
 
         const param1            = this.software[this.pointer + 1];
         const param2            = this.software[this.pointer + 2];
-        const storageLocation   = this.software[this.pointer + 3];
+        const param3            = this.software[this.pointer + 3];
 
-        const param1value = this.getParamValue(param1mode, param1, this.software[param1]);
-        const param2value = this.getParamValue(param2mode, param2, this.software[param2]);
-
-        return { param1mode, param2mode, opcode, param1, param2, storageLocation,
-            param1value, param2value };
+        return { param1mode, param2mode, param3mode, opcode, param1, param2, param3 };
     }
 
     runSoftware() {
@@ -122,8 +165,31 @@ class Computer {
         while (this.pointer < this.software.length && !this.paused) {
              
             let currentState = this.getCurrentState();
-            let {param1, param2, storageLocation} = currentState;
+            let { param1, param2, param3, param1mode, param2mode, param3mode } = currentState;
             this.opcode = currentState.opcode;
+
+            if (this.shouldLog) {
+                if (
+                    this.opcode === '01' || this.opcode === '02' || this.opcode === '07'
+                        || this.opcode === '08'
+                ) {
+                    console.log(this.opcode, `(${param1mode})`, param1, `(${param2mode})`, param2,  `(${param3mode})`, param3);
+                } else if (
+                    this.opcode === '03' || this.opcode === '04' || this.opcode === '09'
+                ) {
+                    console.log(this.opcode, `(${param1mode})`, param1, 'pointer', this.pointer);
+                } else if (
+                    this.opcode === '05' || this.opcode === '06'
+                ) {
+                    console.log(this.opcode,  `(${param1mode})`, param1, `(${param2mode})`, param2, 'pointer', this.pointer);
+                } else {
+                    console.log(this.opcode);
+                }
+                console.log('relativeBase', this.relativeBase);
+                //console.log(this.software);
+                console.log('');
+            }
+
             this.executeInstruction(currentState);
         }
     }
--- javascript/day05p2.js
+++ javascript/day05p2.js
@@ -1,7 +1,12 @@
 const fs = require('fs');
+const Computer = require('./Computer');
+
 const input = fs.readFileSync('../input/day05', 'utf8')
     .trim().split(',').map(string => parseInt(string));
-const Computer = require('./Computer');
+/*
+const input = [3,9,8,9,10,9,4,9,99,-1,8];
+const input = [3,9,7,9,10,9,4,9,99,-1,8]
+*/
 
 
 const intcodeComputer = new Computer([], input, 'thermal air');
